a87aef462a02a8b26d78357917d59d48596434ad
TechnoStrife <technostrife@gmail.com>
2025-05-02 13:51:47 +0300

    Change <leader>D to gt for goto type

--- lua/techno/lsp.lua
+++ lua/techno/lsp.lua
@@ -1,56 +1,56 @@
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-    callback = function(event)
-        local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-        end
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+	callback = function(event)
+		local map = function(keys, func, desc)
+			vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+		end
 
-        --  To jump back, press <C-t>.
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+		--  To jump back, press <C-t>.
+		map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
-        -- Find references for the word under your cursor.
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+		map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+		-- Find references for the word under your cursor.
+		map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+		map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+		map("gt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
 
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+		map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+		map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-        map('<C-x>', vim.lsp.buf.code_action, '[C]ode [A]ction')
+		map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
-        map('K', vim.lsp.buf.hover, 'Hover Documentation')
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { desc = 'LSP: Signature help' })
+		map("<C-x>", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+		map("K", vim.lsp.buf.hover, "Hover Documentation")
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "LSP: Signature help" })
 
-        map('<leader>lr', function()
-            vim.diagnostic.reset()
-            vim.cmd('LspRestart')
-        end, '[L]sp [R]estart')
+		map("<leader>lr", function()
+			vim.diagnostic.reset()
+			vim.cmd("LspRestart")
+		end, "[L]sp [R]estart")
 
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.server_capabilities.documentHighlightProvider then
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-                buffer = event.buf,
-                callback = vim.lsp.buf.document_highlight,
-            })
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
+		if client and client.server_capabilities.documentHighlightProvider then
+			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+				buffer = event.buf,
+				callback = vim.lsp.buf.document_highlight,
+			})
 
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-                buffer = event.buf,
-                callback = vim.lsp.buf.clear_references,
-            })
-        end
+			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+				buffer = event.buf,
+				callback = vim.lsp.buf.clear_references,
+			})
+		end
 
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('<leader>th', function()
-                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oogle inlay hints')
-        end
-        vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
-    end,
+		if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+			map("<leader>th", function()
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+			end, "[T]oogle inlay hints")
+		end
+		vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
+	end,
 })
